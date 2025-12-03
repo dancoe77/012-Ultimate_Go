bcbee021a8ba22999296ba97b696cb415eabedd6
Dan Shoemaker <dan@shoeman-tech.com>
2025-12-02 20:03:29 -0800

    completed module 02

---
+++ 01-Intro_and_Setup/02-Set_up_to_get_started/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Install Go either through your distribution or through the Go website")
+	fmt.Println("Install either VSCode or GoLand to use as the IDE")
+	fmt.Println("Install Git")
+}
