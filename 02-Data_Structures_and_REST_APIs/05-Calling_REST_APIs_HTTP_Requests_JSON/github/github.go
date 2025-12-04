ec8ed1808084a1719b0eb288af01e651eb816b53
Dan Shoemaker <dan@shoeman-tech.com>
2025-12-03 23:45:28 -0800

    completed module 05

---
+++ 02-Data_Structures_and_REST_APIs/05-Calling_REST_APIs_HTTP_Requests_JSON/github/github.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+// Given a github user login, return name and number of public repos
+
+func main() {
+	fmt.Println(UserInfo("dancoe77"))
+}
+func demo() {
+	resp, err := http.Get("https://api.github.com/users/dancoe77")
+	if err != nil {
+		fmt.Println("ERROR:", err)
+		return
+	}
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("ERROR: bad status - %s\n", resp.Status)
+		return
+	}
+
+	ctype := resp.Header.Get("Content-Type")
+	fmt.Println("content-type:", ctype)
+
+	// io.Copy(os.Stdout, resp.Body)
+	var reply struct {
+		Name     string
+		NumRepos int `json:"public_repos"`
+		//Public_Repos int
+	}
+	dec := json.NewDecoder(resp.Body)
+	if err := dec.Decode(&reply); err != nil {
+		fmt.Println("ERROR:", err)
+		return
+	}
+	fmt.Println(reply.Name, reply.NumRepos)
+}
+
+// UserInfo return name and number of public repos from GitHub API.
+func UserInfo(login string) (string, int, error) {
+	url := "https://api.github.com/users/" + login
+	resp, err := http.Get("https://api.github.com/users/dancoe77")
+	if err != nil {
+		return "", 0, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return "", 0, fmt.Errorf("%q - bad status: %s\n", url, resp.Status)
+	}
+	return parseResponse(resp.Body)
+}
+
+func parseResponse(r io.Reader) (string, int, error) {
+	var reply struct {
+		Name     string
+		NumRepos int `json:"public_repos"`
+		//Public_Repos int
+	}
+	dec := json.NewDecoder(r)
+	if err := dec.Decode(&reply); err != nil {
+		return "", 0, err
+	}
+	return reply.Name, reply.NumRepos, nil
+}
+
+/*
+JSON <-> Go
+
+Types
+string <-> string
+true/false <-> gool
+number <-> float64, float32, int, int8 ... int64, uint, uint8 ...
+array <-> []T, []any
+object <-> map[string]any, struct
+
+encoding/json API
+JSON -> []byte -> Go: Unmarshal
+Go -> []byte -> JSON: Marshal
+JSON -> io.Reader -> Go: Decoder
+Go -> io.Writer -> JSON: Encoder
+*/
