e72f7ceb2df8465b56625cc2ca537ebbb04ad7e9
Dan Shoemaker <dan@shoeman-tech.com>
2025-12-31 15:05:34 -0800

    completed module 16

---
+++ 05-Concurrency/16-Overview_of_Concurrency_in_Go/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+var l string = "###################################################"
+
+func main() {
+	fmt.Println(l)
+	fmt.Println("Concurrency is not parallelism")
+	fmt.Println(l)
+}
